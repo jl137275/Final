4eb3361d788203378a2c3daa1b51588de87359f7
dl137273 <hall5776>
2017-12-19 11:18:46 -0600

    update newest changes

--- Final/Final/Rand 8 Team Bracket.Designer.vb
+++ Final/Final/Rand 8 Team Bracket.Designer.vb
@@ -41,6 +41,14 @@ Partial Class Bracket
         Me.lblRound2Seed2 = New System.Windows.Forms.Label()
         Me.lblRound2Seed4 = New System.Windows.Forms.Label()
         Me.lblRound2Seed1 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -73,7 +81,7 @@ Partial Class Bracket
         'txtSeed8
         '
         Me.txtSeed8.BackColor = System.Drawing.Color.LightGray
-        Me.txtSeed8.Location = New System.Drawing.Point(38, 91)
+        Me.txtSeed8.Location = New System.Drawing.Point(37, 91)
         Me.txtSeed8.Name = "txtSeed8"
         Me.txtSeed8.Size = New System.Drawing.Size(145, 20)
         Me.txtSeed8.TabIndex = 12
@@ -81,7 +89,7 @@ Partial Class Bracket
         'txtSeed4
         '
         Me.txtSeed4.BackColor = System.Drawing.Color.LightGray
-        Me.txtSeed4.Location = New System.Drawing.Point(38, 148)
+        Me.txtSeed4.Location = New System.Drawing.Point(38, 150)
         Me.txtSeed4.Name = "txtSeed4"
         Me.txtSeed4.Size = New System.Drawing.Size(145, 20)
         Me.txtSeed4.TabIndex = 13
@@ -89,7 +97,7 @@ Partial Class Bracket
         'txtSeed5
         '
         Me.txtSeed5.BackColor = System.Drawing.Color.LightGray
-        Me.txtSeed5.Location = New System.Drawing.Point(38, 189)
+        Me.txtSeed5.Location = New System.Drawing.Point(37, 190)
         Me.txtSeed5.Name = "txtSeed5"
         Me.txtSeed5.Size = New System.Drawing.Size(145, 20)
         Me.txtSeed5.TabIndex = 14
@@ -97,7 +105,7 @@ Partial Class Bracket
         'txtSeed2
         '
         Me.txtSeed2.BackColor = System.Drawing.Color.LightGray
-        Me.txtSeed2.Location = New System.Drawing.Point(38, 250)
+        Me.txtSeed2.Location = New System.Drawing.Point(37, 248)
         Me.txtSeed2.Name = "txtSeed2"
         Me.txtSeed2.Size = New System.Drawing.Size(145, 20)
         Me.txtSeed2.TabIndex = 15
@@ -121,7 +129,7 @@ Partial Class Bracket
         'txtSeed6
         '
         Me.txtSeed6.BackColor = System.Drawing.Color.LightGray
-        Me.txtSeed6.Location = New System.Drawing.Point(38, 384)
+        Me.txtSeed6.Location = New System.Drawing.Point(38, 391)
         Me.txtSeed6.Name = "txtSeed6"
         Me.txtSeed6.Size = New System.Drawing.Size(145, 20)
         Me.txtSeed6.TabIndex = 18
@@ -148,16 +156,16 @@ Partial Class Bracket
         '
         Me.lblChampion.BackColor = System.Drawing.Color.LightGray
         Me.lblChampion.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblChampion.Location = New System.Drawing.Point(471, 232)
+        Me.lblChampion.Location = New System.Drawing.Point(474, 229)
         Me.lblChampion.Name = "lblChampion"
-        Me.lblChampion.Size = New System.Drawing.Size(156, 23)
+        Me.lblChampion.Size = New System.Drawing.Size(156, 26)
         Me.lblChampion.TabIndex = 34
         '
         'lblround3Seed2
         '
         Me.lblround3Seed2.BackColor = System.Drawing.Color.LightGray
         Me.lblround3Seed2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblround3Seed2.Location = New System.Drawing.Point(310, 317)
+        Me.lblround3Seed2.Location = New System.Drawing.Point(310, 314)
         Me.lblround3Seed2.Name = "lblround3Seed2"
         Me.lblround3Seed2.Size = New System.Drawing.Size(156, 23)
         Me.lblround3Seed2.TabIndex = 33
@@ -166,7 +174,7 @@ Partial Class Bracket
         '
         Me.lblRound3Seed1.BackColor = System.Drawing.Color.LightGray
         Me.lblRound3Seed1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblRound3Seed1.Location = New System.Drawing.Point(310, 121)
+        Me.lblRound3Seed1.Location = New System.Drawing.Point(310, 118)
         Me.lblRound3Seed1.Name = "lblRound3Seed1"
         Me.lblRound3Seed1.Size = New System.Drawing.Size(156, 23)
         Me.lblRound3Seed1.TabIndex = 32
@@ -184,7 +192,7 @@ Partial Class Bracket
         '
         Me.lblRound2Seed2.BackColor = System.Drawing.Color.LightGray
         Me.lblRound2Seed2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblRound2Seed2.Location = New System.Drawing.Point(182, 264)
+        Me.lblRound2Seed2.Location = New System.Drawing.Point(183, 265)
         Me.lblRound2Seed2.Name = "lblRound2Seed2"
         Me.lblRound2Seed2.Size = New System.Drawing.Size(126, 23)
         Me.lblRound2Seed2.TabIndex = 30
@@ -193,7 +201,7 @@ Partial Class Bracket
         '
         Me.lblRound2Seed4.BackColor = System.Drawing.Color.LightGray
         Me.lblRound2Seed4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblRound2Seed4.Location = New System.Drawing.Point(182, 171)
+        Me.lblRound2Seed4.Location = New System.Drawing.Point(183, 168)
         Me.lblRound2Seed4.Name = "lblRound2Seed4"
         Me.lblRound2Seed4.Size = New System.Drawing.Size(126, 23)
         Me.lblRound2Seed4.TabIndex = 29
@@ -202,16 +210,104 @@ Partial Class Bracket
         '
         Me.lblRound2Seed1.BackColor = System.Drawing.Color.LightGray
         Me.lblRound2Seed1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.lblRound2Seed1.Location = New System.Drawing.Point(182, 70)
+        Me.lblRound2Seed1.Location = New System.Drawing.Point(183, 68)
         Me.lblRound2Seed1.Name = "lblRound2Seed1"
         Me.lblRound2Seed1.Size = New System.Drawing.Size(126, 23)
         Me.lblRound2Seed1.TabIndex = 28
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(24, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 35
+        Me.Label1.Text = "1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(14, 13)
+        Me.Label2.TabIndex = 36
+        Me.Label2.Text = "8"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(24, 157)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(14, 13)
+        Me.Label3.TabIndex = 37
+        Me.Label3.Text = "4"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(24, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(14, 13)
+        Me.Label4.TabIndex = 38
+        Me.Label4.Text = "5"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(24, 255)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(14, 13)
+        Me.Label5.TabIndex = 39
+        Me.Label5.Text = "2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(24, 294)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(14, 13)
+        Me.Label6.TabIndex = 40
+        Me.Label6.Text = "7"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(24, 352)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(14, 13)
+        Me.Label7.TabIndex = 41
+        Me.Label7.Text = "3"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(24, 398)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(14, 13)
+        Me.Label8.TabIndex = 42
+        Me.Label8.Text = "6"
         '
         'Bracket
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(886, 457)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblChampion)
         Me.Controls.Add(Me.lblround3Seed2)
         Me.Controls.Add(Me.lblRound3Seed1)
@@ -257,4 +353,12 @@ Partial Class Bracket
     Friend WithEvents lblRound2Seed2 As System.Windows.Forms.Label
     Friend WithEvents lblRound2Seed4 As System.Windows.Forms.Label
     Friend WithEvents lblRound2Seed1 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
 End Class
